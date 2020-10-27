3941396305d31eb2ca2662e070453a562f86d7fc
KStomberg <68118089+KStomberg@users.noreply.github.com>
2020-10-27 12:01:43 -0500

    wrote a program that uses obj, array, func, car, and loops.

--- test.py
+++ test.py
@@ -1,2 +1,20 @@
 msg = "Hello World"
 print(msg)
+
+testArray = ['strOne', 'strTwo', 'strThree']
+print(testArray)
+
+testObj = {'strFour', 'strFive', 'strSix'}
+print(testObj)
+
+def loopTest(container):
+    for string in container:
+        print(letterCounter(string))
+        
+def letterCounter(string):
+    letterCount = len(string)
+    print(letterCount)
+
+loopTest(testArray)
+
+loopTest(testObj)
